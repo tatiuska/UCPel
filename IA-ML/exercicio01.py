6001ec5368f77a70474c63d1ab8c91916a1039f6
tatiuska <tfweska@gmail.com>
2023-03-10 01:22:22 -0300

    pasta Inteligência Artificial - Machine Learning (IA-ML)

---
+++ IA-ML/exercicio01.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+
+iris = load_iris()
+x = iris.data[:, 2:] #comprimento e largura das pétalas
+y = iris.target
+
+tree_clf = DecisionTreeClassifier(max_depth=2)
+tree_clf.fit(x, y)
+
+from sklearn.tree import export_graphviz
+
+export_graphviz(
+    tree_clf,
+    out_file=image_path("iris_tree.dot"),
+    feature_names=iris.feature_names[2:],
+    class_names=iris.target_names,
+    rounded=True,
+    filled=True
+)
