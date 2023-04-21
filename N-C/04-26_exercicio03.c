03d201a337c5be82bbb96c6deac252ae3329ca29
tatiuska <tfweska@gmail.com>
2023-04-21 20:59:01 -0300

    finalização Exercício 03

--- N-C/04-26_exercicio03.c
+++ N-C/04-26_exercicio03.c
@@ -1,53 +1,44 @@
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
+
 //arrumar codigo
 int main(){
 
-//variaveis
-int altura;
-float peso;
-char classificacao;
+    //variaveis
+    int altura;
+    float peso;
+    char classificacao;
 
-//dados de entrada
-printf("Informe sua altura (em cm): \n");
-scanf("%d", &altura);
+    //dados de entrada
+    printf("Informe sua altura (em cm): \n");
+    scanf("%d", &altura);
 
-printf("Informe seu peso (em kg): \n");
-scanf("%.2f", &peso);
+    printf("Informe seu peso (em kg): \n");
+    scanf("%f", &peso);
 
-//processamento
-if(altura < 120) and (peso <= 60); {
-    classificacao = "A";
-}
-if else (altura > 120, <= 170, peso <= 60){
-    classificacao = "B";
-}
-if else (altura > 170) and (peso <= 60){
-    classificacao = "C";
-}
-if else (altura < 120) and (peso > 60 and <= 90){
-    classificacao = "D";
-}
-if else (altura > 120 and <= 170) and (peso > 60 and <= 90){
-    classificacao = "E";
-}
-if else (altura > 170) and (peso > 60 and <= 90){
-    classificacao = "F";
-}
-if else (altura < 120) and (peso > 90){
-    classificacao = "G";
-}
-if else (altura > 120 and <= 170) and (peso > 90){
-    classificacao = "H";
-}
-if else (altura > 170) and (peso > 90){
-    classificacao = "I";
-}
-else {
-    printf("Valores inválidos!");
-}
+    //processamento
+    if(altura < 120 && peso <= 60) {
+        classificacao = 'A';
+    } else if((altura > 120 && altura <= 170) && (peso <= 60)){
+        classificacao = 'B';
+        } else if(altura > 170 && peso <= 60){
+            classificacao = 'C';
+            } else if((altura < 120) && (peso > 60 && peso <= 90)){
+                classificacao = 'D';
+                } else if((altura > 120 && altura <= 170) && (peso > 60 && peso <= 90)){
+                    classificacao = 'E';
+                    } else if((altura > 170) && (peso > 60 && peso <= 90)){
+                        classificacao = 'F';
+                        } else if(altura < 120 && peso > 90){
+                            classificacao = 'G';
+                            } else if((altura > 120 && altura <= 170) && (peso > 90)){
+                                classificacao = 'H';
+                                } else if(altura > 170 && peso > 90){
+                                    classificacao = 'I';}
 
-//dados de saida
-printf("Sua altura eh %d, seu peso eh %.2f e sua classificacao eh %c", altura, peso, classificacao);
+    //dados de saida
+    printf("Sua altura eh %dcm, seu peso eh %.2fkg e sua classificacao eh %c.\n", altura, peso, classificacao);
 
-return 0;
+    return 0;
 }
