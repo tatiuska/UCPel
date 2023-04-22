99064909788ee5a100ee8f6dc70ce5f368b70cbe
tatiuska <tfweska@gmail.com>
2023-04-22 18:28:14 -0300

    ajuste Exercício 02 (26-04)

--- N-C/04-26_exercicio02.c
+++ N-C/04-26_exercicio02.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
+//programa que le o nivel de um professor e a quantidade de horas-aula ministrada para calculo do salario
+
 int main(){
 
-//variáveis
-int nivel, hora_aula;
-float salario;
+    //variáveis
+    int nivel, hora_aula;
+    float salario;
 
-//dados de entrada
-printf("Informe seu Nivel: \n 1 para Nivel 1\n 2 para Nivel 2\n 3 para Nivel 3\n");
-scanf("%d", &nivel);
+    //dados de entrada
+    printf("Informe seu Nivel: \n1 - Nivel 1 \n2 - Nivel 2 \n3 - Nivel 3 \n");
+    scanf("%d", &nivel);
 
-printf("Informe a quantidade de horas/aula trabalhadas: \n");
-scanf("%d", &hora_aula);
+    printf("Informe a quantidade de horas/aula trabalhadas: \n");
+    scanf("%d", &hora_aula);
 
-//processamento
+    //processamento
     switch(nivel) {
 
     case 1:
@@ -33,10 +35,10 @@ scanf("%d", &hora_aula);
 
     }
 
-//dados de saída
-printf("Seu Nivel eh %d e seu salario eh de R$ %.2f", nivel, salario);
+    //dados de saída
+    printf("Seu Nivel eh %d e seu salario eh de R$ %.2f", nivel, salario);
 
-return 0;
+    return 0;
 }
 
 
