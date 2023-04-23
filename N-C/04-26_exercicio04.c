e91f3455a1b8e94bae5c3f5c04ba391947256c0a
tatiuska <tfweska@gmail.com>
2023-04-22 22:21:49 -0300

    criação arquivos Exercícios 5 e 6, finalização Exercício 4

--- N-C/04-26_exercicio04.c
+++ N-C/04-26_exercicio04.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 
-//arrumar a parte do char e do if referente a isso
+//programa para ler o codigo de comida e bebida de um cardapio e a quantidade de cada um.
+//calcular o valor a ser pago pelo cliente.
 
 int main(){
 
@@ -14,47 +15,66 @@ int main(){
 
 
     //dados de entrada
-    printf("Digite o codigo da comida e a quantidade: \n100 - Cachorro Quente \n101 - Bauru Simples \n102 - Bauru com Ovo \n103 - Hamburguer \n104 - Cheeseburguer \n");
-    scanf("%d %d", &cod_c, &qt_c);
+    printf("Digite o codigo da comida que deseja: \n100 - Cachorro Quente \n101 - Bauru Simples \n102 - Bauru com Ovo \n103 - Hamburguer \n104 - Cheeseburguer \n\n");
+    scanf("%d", &cod_c);
 
-    printf("Digite o codigo da bebida e a quantidade: \n105 - Suco \n106 - Refrigerante\n");
-    scanf("%d %d", &cod_b, &qt_b);
+    printf("Escolha a quantidade da comida escolhida: \n\n");
+    scanf("%d", &qt_c);
+
+    printf("Digite o codigo da bebida que deseja: \n105 - Suco \n106 - Refrigerante\n\n");
+    scanf("%d", &cod_b);
+
+    printf("Escolha a quantidade da bebida escolhida: \n\n");
+    scanf("%d", &qt_b);
 
     //processamento
-    if(cod_c = 100){
-        comida = 'Cachorro Quente';
+    //switch/case para escolha da comida
+    switch(cod_c){
+    case 100:
+        strcpy(comida, "Cachorro Quente");
         preco_c = 1.20 * qt_c;
-    } else if(cod_c = 101){
-        comida = getchar();
+    break;
+    case 101:
+        strcpy(comida, "Bauru Simples");
         preco_c = 1.30 * qt_c;
-        } else if(cod_c = 102){
-            comida = 'Bauru com Ovo';
-            preco_c = 1.50 * qt_c;
-            } else if(cod_c = 103){
-                comida = 'Hamburguer';
-                preco_c = 1.20 * qt_c;
-                } else if(cod_c = 104){
-                    comida = 'Cheeseburguer';
-                    preco_c = 1.70 * qt_c;
-                    } else{
-                        printf("Digite um codigo valido!!");
-                        }
+    break;
+    case 102:
+        strcpy(comida, "Bauru com Ovo");
+        preco_c = 1.50 * qt_c;
+    break;
+    case 103:
+        strcpy(comida, "Hamburguer");
+        preco_c = 1.20 * qt_c;
+    break;
+    case 104:
+        strcpy(comida, "Cheeseburguer");
+        preco_c = 1.70 * qt_c;
+    break;
+    default:
+        printf("Codigo invalido!!\n\n");
+    };
 
-    if(cod_b = 105){
-        bebida = 'Suco';
+    //switch/case para escolha da bebida (poderia ter mantido um if/else).
+    switch(cod_b){
+    case 105:
+        strcpy(bebida, "Suco");
         preco_b = 2.20 * qt_b;
-    } else if(cod_b = 106){
-        bebida = 'Refrigerante';
+    break;
+    case 106:
+        strcpy(bebida, "Refrigerante");
         preco_b = 1.00 * qt_b;
-        } else {
-            printf("Digite um codigo valido!!");
-            }
+    break;
+    default:
+        printf("Codigo invalido!\n\n");
+    };
 
+    //calculo do preco a pagar
     preco_final = preco_c + preco_b;
 
 
     //dados de saida
-    printf("Seu pedido eh: %c(%d), %c(%d) e o valor a ser pago eh: R$ %.2f\n", comida, qt_c, bebida, qt_b, preco_final);
+    printf("Seu pedido: %s (%d), %s (%d)\n", comida, qt_c, bebida, qt_b);
+    printf("Valor a ser pago: R$ %.2f\n", preco_final);
 
     return 0;
 }
---
+++ N-C/04-26_exercicio06.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//
+int main(){
+
+    //variaveis
+
+
+
+    //dados de entrada
+
+
+
+    //processamento
+
+
+
+
+    //dados de saida
+
+    return 0;
+}
