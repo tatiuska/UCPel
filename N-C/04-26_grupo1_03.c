7ab3aa65f8665a9f7ca03d64d5d2d7c60bdd3377
tatiuska <tfweska@gmail.com>
2023-05-03 18:46:16 -0300

    update exercício 3 (grupo 2)

--- N-C/04-26_grupo1_03.c
+++ N-C/04-26_grupo1_03.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+ int main(){
+
+    //variaveis
+    int idade, media_gr, media_m, media_m;
+    char sexo;
+
+    //dados de entrada
+    printf("Informe sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("Informe seu sexo: 1 - Masculino \n2 - Feminino \n");
+    scanf("%c", &sexo);
+
+    //processamento
+    for(idade>0; idade)
+
+
+
+    //dados de saida
+
+
+
+    return 0;
+ }
