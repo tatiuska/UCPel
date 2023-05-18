c997f58ce5e995ecefe0029c4654acc268f85650
tatiuska <tfweska@gmail.com>
2023-05-18 15:55:47 -0300

    finalização Exercício Final (17/05)

--- N-C/05-17_exercicio_final.c
+++ N-C/05-17_exercicio_final.c
@@ -2,22 +2,22 @@
 #include <string.h>
 #include <ctype.h>
 
-#define TAMANHO_MAXIMO 100
+#define TAMANHO_MAXIMO 100 // definição de tamanho máximo para o vetor
 
 int main() {
-    //variaveis
+    // variaveis
     char senha[TAMANHO_MAXIMO];
     int tamanho;
     int tem_maiuscula = 0;
     int tem_minuscula = 0;
     int tem_digito = 0;
 
-    //dados de entrada
+    // dados de entrada
     printf("Digite a senha: ");
     fgets(senha, sizeof(senha), stdin);
     tamanho = strlen(senha) - 1; // Subtrai 1 para excluir o caractere de nova linha (\n)
 
-    //processamento
+    // processamento
     // verificar se a senha atende aos criterios
     for (int i = 0; i < tamanho; i++) {
         if (isupper(senha[i])) {
@@ -29,7 +29,7 @@ int main() {
         }
     }
 
-    //processamento e dados de saida
+    // processamento e dados de saida
     // verificar se a senha e forte o suficiente
     if (tamanho >= 8 && tem_maiuscula && tem_minuscula && tem_digito) {
         printf("A senha eh forte.\n");
