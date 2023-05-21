a059ab2d047f0326455faf4f32e52b63593b7905
tatiuska <tfweska@gmail.com>
2023-05-21 18:11:24 -0300

    add comentários no programa

--- POO/VehicleDemo/src/vehicledemo/VehicleDemo.java
+++ POO/VehicleDemo/src/vehicledemo/VehicleDemo.java
@@ -6,15 +6,18 @@ class Vehicle {
     int mpg; //consumo em milhas por galão
         
 }
+//declarando um objeto do tipo Vehicle
 class VehicleDemo {    
     public static void main(String[] args) {
         Vehicle minivan = new Vehicle();
         int range;
         
+        //atribuindo valores aos campos de minivan
         minivan.passengers = 7;
         minivan.fuelcap = 16;
         minivan.mpg = 21;
         
+        //cálculo de autonomia a partir de um tanque cheio de gasolina
         range = minivan.fuelcap * minivan.mpg;
         System.out.println("Minivan can carry " + minivan.passengers + " with a range of " + range);
     }
