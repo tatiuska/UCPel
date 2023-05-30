9a8b93011f0c90118073e878585d42f00c942967
tatiuska <tfweska@gmail.com>
2023-05-30 13:53:45 -0300

    Criação de arquivos para tarefa S5 (index.html, conversor.php e style.css

---
+++ PExtDS-23/S5/conversor.php
@@ -0,0 +1,22 @@
+<!-- processamento da conversão de modedas escolhidas pelo usuário -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversão de Moedas</title>
+    <!-- link para carregamento do sytle.css -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Resultado</h1>
+    </header>
+    <main>
+        <?php 
+            
+        ?>
+    </main>
+</body>
+</html>
