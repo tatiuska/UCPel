b0b91862478bff1ead928c807841ef337eaca78a
tatiuska <tfweska@gmail.com>
2023-06-11 21:35:59 -0300

    modificação classe Controle.java (Desafio UA 08)

--- POO/Escola/src/escola/Controle.java
+++ POO/Escola/src/escola/Controle.java
@@ -1,20 +1,21 @@
 package escola;
 
 public class Controle {
+
     public static void main(String[] args) {
-        PF pessoafisica1; 
-        pessoafisica1 = new PF();
-        pessoafisica1.nome = setNome("Julia Almeida");
-        pessoafisica1.cpf = setCpf("876.768.348-22");
-        pessoafisica1.rg = setRg("1676228934");
-        pessoafisica1.titulacao = setTitulacao("Doutorado em Engenharia da Computação");
-        pessoafisica1.logradouro = setLogradouro("Rua Andrades Neves");
-        pessoafisica1.numero = setNumero("10765");
-        pessoafisica1.cidade = setCidade("Pelotas");
-        pessoafisica1.uf = setUf("RS");
-        pessoafisica1.disciplina = setDisciplina("Programação Orientada a Objetos");
+        PF pf1; 
+        pf1 = new PF();
+        pf1.nome = setNome("Julia Almeida");
+        pf1.cpf = setCpf("876.768.348-22");
+        pf1.rg = setRg("1676228934");
+        pf1.titulacao = setTitulacao("Doutorado em Engenharia da Computação");
+        pf1.logradouro = setLogradouro("Rua Andrades Neves");
+        pf1.numero = setNumero(10765);
+        pf1.cidade = setCidade("Pelotas");
+        pf1.uf = setUf("RS");
+        pf1.disciplina = setDisciplina("Programação Orientada a Objetos");
         
-        System.out.println("A colaboradora" + pessoafisica1.getNome() + "é professora de " + pessoafisica1.getDisciplina() ".");
+        System.out.println("A colaboradora" + getNome(nome) + "é professora de " + getDisciplina(disciplina) + ".");
         
     }
     
