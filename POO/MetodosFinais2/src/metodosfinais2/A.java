020e6e82d2e5e98e443ecde651726b93f97457f4
tatiuska <tfweska@gmail.com>
2023-06-12 19:21:16 -0300

    Dica do Professor UA 12 (Atributos e Métodos Finais)

---
+++ POO/MetodosFinais2/src/metodosfinais2/A.java
@@ -0,0 +1,11 @@
+package metodosfinais2;
+
+public class A {
+    //Atributo final
+    private final String texto = "Valor imutável!";
+    
+    //Método final
+    public final void imprimeTexto(){
+        System.out.println(texto);
+    }
+}
---
+++ POO/MetodosFinais2/src/metodosfinais2/MetodosFinais2.java
@@ -0,0 +1,10 @@
+package metodosfinais2;
+
+public class MetodosFinais2 {
+
+    public static void main(String [] args) {
+        A i = new A();
+        i.imprimeTexto();
+    }
+    
+}
