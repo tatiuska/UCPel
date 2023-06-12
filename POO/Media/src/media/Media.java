81b3a036e4e20848b99c40fbbfb9f74fef6fd740
tatiuska <tfweska@gmail.com>
2023-06-11 21:05:04 -0300

    correção Exercício Dica do Professor (UA 01)

--- POO/Media/src/media/Media.java
+++ POO/Media/src/media/Media.java
@@ -2,20 +2,19 @@
 package media;
 
 public class Media {
-    public static void main(String[] args) {
-        private double nota1;
-        private double nota2;
-        private double media;
+    
+    private double nota1;
+    private double nota2;
+    private double media;
         
-        public void calculaMedia(double nota1, double nota2){
-            this.nota1 = nota1;
-            this.nota2 = nota2;
+    public void calculaMedia(double nota1, double nota2){
+        this.nota1 = nota1;
+        this.nota2 = nota2;
             
-            media = (nota1 + nota2)/2;        
-        }
+        media = (nota1 + nota2)/2;        
+    }
         
-        public double retornaMedia(){
-            return media;
-        }
+    public double retornaMedia(){
+        return media;
     }
 }
