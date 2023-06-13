18d306c09e3ef8d48a491bc1c1233768a767ded4
tatiuska <tfweska@gmail.com>
2023-06-13 15:15:43 -0300

    ajuste dos campos do banco de dados para coincidir com os nomes das variávies criadas no PHP

--- PExtDS-23/S7/sql/alunos_cad.sql
+++ PExtDS-23/S7/sql/alunos_cad.sql
@@ -3,9 +3,9 @@ USE alunos_cad;
 CREATE table alunos(
 	nome varchar(30),
     cpf int,
-    data_nasc date,
-    num_matr int,
-    sem_ingresso varchar(8),
+    d_nasc date,
+    matr int,
+    s_ingresso varchar(8),
     curso varchar(50)
 );
 DESCRIBE table alunos;
