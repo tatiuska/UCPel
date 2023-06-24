101e12bfefc031c88e0703fa3f244d28d24a111a
tatiuska <tfweska@gmail.com>
2023-06-24 14:18:42 -0300

    início implementação Final PI-III-B

---
+++ PI-III-B/2023-06-24/index.php
@@ -0,0 +1,28 @@
+<!-- Página Incial: Tela de Login -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IMDAZ - Sistema de Gerenciamento de Pessoas</title>
+    <!-- CSS Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <!-- JS Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
+
+</head>
+    <?php 
+        // Inserir código de conexão com o servidor
+    ?>
+<body>
+    <!-- Navbar com menu -->
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                Instituto de Menores Dom Antônio Zattera 
+            <img src="./img/logo_IMDAZ.svg" alt="Logo" width="128" height="58" class="d-inline-block align-text-top">
+            </a>
+        </div>
+    </nav>
+</body>
+</html>
