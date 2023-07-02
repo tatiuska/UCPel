c5761c304d8c4da733c2311c36050ed67d7c6454
tatiuska <tfweska@gmail.com>
2023-07-02 01:02:07 -0300

    atualizações css e php

--- PI-III-B/2023-06-24/index.php
+++ PI-III-B/2023-06-24/index.php
@@ -13,55 +13,45 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
 </head>
     <?php 
-        // Inserir código de conexão com o servidor
+        include "./php/conexao.php";
     ?>
 <body>
-    <header>
-        <div class="container-fluid" id="header-navbar">
-            <!-- Header e Navbar -->
-            <nav class="navbar navbar-expand-sm d-flex p-2">
-                <a class="navbar-brand" href="#">
-                    <p>
-                        Instituto de Menores
-                        Dom Antônio Zattera
-                    </p> 
-                    <img src="./img/logo_IMDAZ.png" alt="Logo" width="128" height="58" class="d-inline-block align-text-top">
-                </a>
-                <ul class="nav navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a class="nav-item nav-link" href="#" id="contato-menu">Entrar</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+    <!-- Header e Navbar -->
+    <header class="navbar navbar-expand-lg bd-navbar sticky-top">
+        <nav class="container-xxl bd-gutter flex-wrap flex-lg-nowrap" aria-label="Menu de navegação">
+            <a class="navbar-brand" href="#">
+                <p>
+                    Instituto de Menores
+                    Dom Antônio Zattera
+                </p> 
+                <img src="./img/logo_IMDAZ.png" alt="Logo" width="128" height="58" class="d-inline-block align-text-top">
+            </a>
+            <ul class="nav navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-item nav-link" href="#" id="contato-menu">Entrar</a>
+                </li>
+            </ul>
+        </nav>
     </header>
     <main>
-        <h1>Sistema de Gestão de Pessoas</h1>
-        <!-- Fim da página -->
-        <footer>
-            <div class="container-fluid" id="footer">
-                <div class="row" id="footer-row">
-                    <div class="col-4" id="social-media">
-                        <p>
-                            <a href="https://github.com/tatiuska" target="_blank" alt="GitHub: tatiuska">
-                                <i class="bi bi-github"></i>
-                            </a> 
-                            <a href="https://linkedin.com/in/tatiana-weska" target="_blank" alt="Linkedin: Tatiana Weska">
-                                <i class="bi bi-linkedin"></i>
-                            </a> 
-                            <a href="https://medium.com/@tatiuska" target="_blank" alt="Medium: @tatiuska">
-                                <i class="bi bi-medium"></i>
-                            </a>
-                        </p>
-                    </div>
-                    <div class="col-4" id="conteudo-final">
-                        <p>
-                            2023 &copy; Projeto Integrador III-B. Desenvolvido por Eduarda Quintana, Leonardo Treichel e Tatiana Weska.
-                        </p>
-                    </div>
+        <div class="container" id="login">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Sistema de Gestão de Pessoas</h1>
+                    <p>Faça seu login aqui:</p>
                 </div>
             </div>
-        </footer>
+        </div>
     </main>
+    <!-- Fim da página -->
+    <footer class="footer">
+        <div class="container">
+            <div class="row" id="footer-row">
+                <div class="col-12" id="conteudo-final">
+                    <p>2023 &copy; Projeto Integrador III-B. Desenvolvido por Eduarda Quintana, Leonardo Treichel e Tatiana Weska.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
