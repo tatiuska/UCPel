b65484e6e419ddb3ad5492d51624b2d4b82c1871
tatiuska <tfweska@gmail.com>
2023-09-07 18:39:19 -0300

    pasta exercícios desenvolvimento web (UA 10)

---
+++ FDW/UA 10/js/situacao.js
@@ -0,0 +1,8 @@
+function mostraSituacao(media){
+    if(media >= 7){
+        alert("Aprovado!");
+    }
+    else {
+        alert("Reprovado!!!1111!ONZE");
+    }
+}
---
+++ FDW/UA 10/js/soma.js
@@ -0,0 +1,8 @@
+// Exemplo de função para somar dois números
+function soma(){
+    var numero1 = 2;
+    var numero2 = 5;
+    var soma = 0;
+    soma = numero1 + numero2;
+    return soma;
+}
