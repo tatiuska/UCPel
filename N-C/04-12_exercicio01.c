4057dd6927f4a28843a468cffe2d64f46b2c9f83
tatiuska <tfweska@gmail.com>
2024-04-08 12:21:28 -0300

    adição de comentários no código dos exercícios do curso de C (2023)

--- N-C/04-12_exercicio01.c
+++ N-C/04-12_exercicio01.c
@@ -1,18 +1,23 @@
+// Crie um programa em C para realizar a soma de dois números através da leitura de dados do teclado.
 #include <stdio.h>
-
 
 int main() {
 
+    // variáveis
     int num1, num2, resultado;
 
+    // solicitando o primeiro número da soma
     printf("Insira o primeiro numero: \n");
     scanf("%d", &num1);
 
+    // solicitando o segundo número da soma
     printf("Insira o segundo numero: \n");
     scanf("%d", &num2);
 
+    // cálculo da soma
     resultado = num1 + num2;
 
+    // impressão do resultado
     printf("O resultado da soma e: %d\n", resultado);
 
     return 0;
