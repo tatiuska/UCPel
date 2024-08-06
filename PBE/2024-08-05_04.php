ed65a5ca2b8aa77d61bfd887cb5307b7f5048507
tatiuska <tfweska@gmail.com>
2024-08-05 21:11:01 -0300

    Demonstração de como definir constantes em PHP

---
+++ PBE/2024-08-05_04.php
@@ -0,0 +1,12 @@
+<?php 
+    // Demonstração de como funcionam as constantes em PHP
+    define('SAUDACAO', 'Hello World');
+    echo SAUDACAO;
+    echo "</br>"
+?>
+
+<?php 
+    // Outra forma de definir uma constante em PHP
+    const SAUDAR = 'Hello, World!';
+    echo SAUDAR;
+?>
