829a4f06d1470a8cf495186345212f480174a4ca
tatiuska <tfweska@gmail.com>
2024-08-06 17:22:51 -0300

    Estruturas de controle: if/else (UA 02)

---
+++ PBE/2024-08-06_06.php
@@ -0,0 +1,11 @@
+<?php 
+    // Estruturas de controle: if/else
+    $idade = 18;
+
+    if($idade >= 18) {
+        echo "Vocé é maior de idade.";
+    }
+    else {
+        echo "Você é menor de idade.";
+    }
+?>
