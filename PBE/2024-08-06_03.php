31eac48cdd5bfb3700dbe3353d875606c699bedd
tatiuska <tfweska@gmail.com>
2024-08-06 10:30:19 -0300

    Desafio UA 02

---
+++ PBE/2024-08-06_03.php
@@ -0,0 +1,13 @@
+<?php 
+    // Desafio UA 02
+    $nome = "João Pedro Dias";
+    $idade = 15;
+
+    echo "Nome completo: " . $nome . ". </br>";
+
+    if($idade < 18)
+        echo "Idade: " . $idade . " anos (menor de idade)." . "</br>";
+    else
+        echo "Idade: " . $idade . " anos (maior de idade)." . "</br>";
+
+?>
