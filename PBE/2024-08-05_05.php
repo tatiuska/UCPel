c98c0209aef11865825a2716828f66da764737d2
tatiuska <tfweska@gmail.com>
2024-08-05 21:17:38 -0300

    exemplos de uso da função var_dump()

---
+++ PBE/2024-08-05_05.php
@@ -0,0 +1,12 @@
+<?php 
+    // Função var_dump()
+    $a = array(3, 5, array("aaa", "bbb", "ccc"));
+    var_dump($a);
+?>
+
+<?php 
+    // Outro exemplo do uso da função var_dump()
+    $preco = 100.75;
+    $barato = false;
+    var_dump($preco, $barato);
+?>
