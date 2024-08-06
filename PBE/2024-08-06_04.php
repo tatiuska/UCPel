1ab69e62e8a6455af56d0ef7dfed0d77a4438ed9
tatiuska <tfweska@gmail.com>
2024-08-06 16:36:18 -0300

    exercício de operadores aritméticos (UA 02)

---
+++ PBE/2024-08-06_04.php
@@ -0,0 +1,24 @@
+<?php 
+    // Usando operadores aritméticos (UA 02)
+    $a = 5;
+    $b = 2;
+
+    $soma = $a + $b;
+    echo "O resultado da soma entre " . $a . " e " . $b . " é: " . $soma . "</br>";
+
+    $subtracao = $a - $b;
+    echo "O resultado da substração entre " . $a . " e " . $b . " é: " . $subtracao . "</br>";
+
+    $multiplicacao = $a * $b;
+    echo "O resultado da multiplicação entre " . $a . " e " . $b . " é: " . $multiplicacao . "</br>";
+
+    $divisao = $a / $b;
+    echo "O resultado da divisão entre " . $a . " e " . $b . " é: " . $divisao . "</br>";
+
+    $modulo = $a % $b;
+    echo "O resultado do módulo entre " . $a . " e " . $b . " é: " . $modulo . "</br>";
+
+    $exponenciacao = $a ** $b;
+    echo "O resultado da exponenciação entre " . $a . " e " . $b . " é: " . $exponenciacao . "</br>";
+
+?>
