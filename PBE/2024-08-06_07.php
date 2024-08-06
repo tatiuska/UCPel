9c0739391200e0194efff39d2f3701a6e8a91a25
tatiuska <tfweska@gmail.com>
2024-08-06 17:27:50 -0300

    Estruturas de controle: switch/case (UA 02)

---
+++ PBE/2024-08-06_07.php
@@ -0,0 +1,35 @@
+<?php 
+    // Estruturas de controle Switch/Case
+    $diaSemana = 4;
+
+    switch($diaSemana) {
+        case 1:
+            echo "Hoje é segunda-feira";
+        break;
+        
+        case 2:
+            echo "Hoje é terça-feira";
+        break;
+        
+        case 3:
+            echo "Hoje é quarta-feira";
+        break;
+
+        case 4:
+            echo "Hoje é quinta-feira";
+        break;
+
+        case 5:
+            echo "Hoje é sexta-feira";
+        break;
+
+        case 6:
+            echo "Hoje é sábado";
+        break;
+
+        case 7:
+            echo "Hoje é domingo";
+        break;
+
+    }
+?>
