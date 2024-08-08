229f223a454d99f650e1bc0284725b4370d5d776
tatiuska <tfweska@gmail.com>
2024-08-08 19:28:34 -0300

    correção no arquivo 2024-08-08_07.php (UA 03)

--- PBE/2024-08-08_07.php
+++ PBE/2024-08-08_07.php
@@ -4,5 +4,8 @@
     session_start();
 
     // armazenando na sessão
-    echo 'Seja bem-vindo, ' . $_SESSION["nomeusuario"] = $_POST["nomeusuario"] . '!';
+    $_SESSION["nomeusuario"] = $_POST["nomeusuario"] . '!';
+
+    // a veriável $_SESSION["nomeusuario"] pode ser usada em qualquer script em que se inicialize o uso de sessões em qualquer outro arquivo
+    echo 'Seja bem-vindo, ' . $_SESSION["nomeusuario"] . '!';
 ?>
