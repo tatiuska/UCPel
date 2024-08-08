6299db70eda1e8df24c937989761cf78df0c82e1
tatiuska <tfweska@gmail.com>
2024-08-08 19:15:00 -0300

    complementando um script com um echo para imprimir o custo total do pedido

--- PBE/2024-08-08_06.php
+++ PBE/2024-08-08_06.php
@@ -2,4 +2,6 @@
     // Usando a função setcookie
     $custototal = 580;
     setcookie("CustoTotalPedido", $custototal, time()+7200);
+
+    echo "Custo Total: R$ " . $_COOKIE["CustoTotalPedido"];
 ?>
