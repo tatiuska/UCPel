651bc8e9bd331e94a8de0742443516f6e88e41a9
tatiuska <tfweska@gmail.com>
2024-08-08 15:38:40 -0300

    uso de variável global (UA 03)

--- PBE/2024-08-08_03.php
+++ PBE/2024-08-08_03.php
@@ -3,11 +3,12 @@
     $custodofrete = 30;
     function totalpagamento($totalprodutos, $frete) {
         $custototal = 0; // Variável local no escopo interno da função
+        global $custodofrete; // $custodofrete terá escopo global, passando a ter o valor da variável $custodofrete externa
         for($indiceprod=0; $indiceprod<3; $indiceprod++) {
             $custototal = $custototal + $totalprodutos[$indiceprod];
         }
         $custototal += $frete;
-        echo 'Custo do frete: ' . $custodofrete; // $custodofrete não é reconhecida 
+        echo 'Custo do frete: R$ ' . $custodofrete . "</br>"; // $custodofrete não é reconhecida 
         return $custototal;
     }
     $produtos = array(50, 200, 300);
