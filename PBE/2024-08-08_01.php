c2f44bd66255aa49c622d623bd6ae3acbdee51d2
tatiuska <tfweska@gmail.com>
2024-08-08 12:06:13 -0300

    versão da função com mudança de parâmetro na chamada e outra com a predefinição de valor de parâmetro direto na composição da função

--- PBE/2024-08-08_01.php
+++ PBE/2024-08-08_01.php
@@ -10,5 +10,35 @@
     }
 
     $produtos = array(50, 200, 300);
-    echo "Custo total: R$ " . totalpagamento($produtos, 30);
+    echo "Custo total: R$ " . totalpagamento($produtos, 30) . "</br>";
+?>
+
+<?php 
+    function totalpagamento2($totalprodutos, $frete) {
+        $custototal = 0;
+        for($indiceprod=0; $indiceprod<count($totalprodutos); $indiceprod++) {
+            $custototal = $custototal + $totalprodutos[$indiceprod];
+        }
+        $custototal += $frete;
+        return $custototal;
+    }
+
+    $produtos = array(50, 200, 300);
+    // Mudando o valor do frete na chamada da função
+    echo "Custo total: R$ " . totalpagamento2($produtos, 50). "</br>";
+?>
+
+<?php 
+    // Predefinindo o valor do parâmetro $frete na função)
+    function totalpagamento3($totalprodutos, $frete=70) {
+        $custototal = 0;
+        for($indiceprod=0; $indiceprod<count($totalprodutos); $indiceprod++) {
+            $custototal = $custototal + $totalprodutos[$indiceprod];
+        }
+        $custototal += $frete;
+        return $custototal;
+    }
+
+    $produtos = array(50, 200, 300);
+    echo "Custo total: R$ " . totalpagamento3($produtos). "</br>";
 ?>
