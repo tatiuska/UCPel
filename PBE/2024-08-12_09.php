f3ae09f0be53c804663a9fd95dfab65bb5c9dcf5
tatiuska <tfweska@gmail.com>
2024-08-12 18:19:33 -0300

    Estruturas de repetição - For - duas opções de sintaxe (UA 04)

---
+++ PBE/2024-08-12_09.php
@@ -0,0 +1,12 @@
+<?php 
+    // Estruturas de repetição - For (UA 04)
+    for($i=1; $i<=10; $i++) {
+        echo $i;
+    }
+
+    echo "</br>";
+    
+    for($i=1; $i<=10; $i++):
+        echo $i;
+    endfor;
+?>
