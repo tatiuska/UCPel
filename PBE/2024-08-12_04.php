6d7e0a29ace265a37b9f652ba0c348b6a0ed1c39
tatiuska <tfweska@gmail.com>
2024-08-12 17:49:00 -0300

    Estruturas condicionais - Elseif (UA 04)

---
+++ PBE/2024-08-12_04.php
@@ -0,0 +1,13 @@
+<?php 
+    // Estruturas condicionais - Elseif (UA 04)
+    $x = 1;
+    $y = 2;
+
+    if($x == $y) {
+        echo "$x e $y são iguais.";
+    } elseif($x > $y) {
+        echo "$x é maior que $y.";
+    } elseif($x < $y) {
+        echo "$x é menor que $y.";
+    }
+?>
