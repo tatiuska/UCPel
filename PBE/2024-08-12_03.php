a25db79f5530afa3310e2c1df169feab9b9db612
tatiuska <tfweska@gmail.com>
2024-08-12 17:48:15 -0300

    Estruturas condicionais - Else (UA 04)

---
+++ PBE/2024-08-12_03.php
@@ -0,0 +1,11 @@
+<?php 
+    // Estruturas condicionais - Else (UA 04)
+    $x = 1;
+    $y = 2;
+
+    if($x == $y) {
+        echo "$x e $y são iguais.";
+    } else {
+        echo "$x e $y são diferentes.";
+    }
+?>
