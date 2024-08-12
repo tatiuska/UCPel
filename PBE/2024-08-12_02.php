0d279afbf6c7f251930961e53fc15e94e6983f3d
tatiuska <tfweska@gmail.com>
2024-08-12 17:42:57 -0300

    estrutura condicional - If com operadores lógicos (UA 04)

---
+++ PBE/2024-08-12_02.php
@@ -0,0 +1,11 @@
+<?php 
+    // Estruturas condicionais: if com uso de operadores lógicos
+    $media = 8.2;
+
+    if(($media >= 8) and ($media < 9)) {
+        $conceito = "B";
+    }
+
+    echo "Seu conceito é $conceito.";
+
+?>
