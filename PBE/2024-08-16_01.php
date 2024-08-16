1bdb5f2403bd20fa7d3158302a74b665f1740d7e
tatiuska <tfweska@gmail.com>
2024-08-16 17:40:43 -0300

    funções - UA 05

---
+++ PBE/2024-08-16_01.php
@@ -0,0 +1,11 @@
+<?php  
+    // Funções - UA 05
+    function multiplica($arg1, $arg2) {
+        $retorno = $arg1 * $arg2;
+        return $retorno;
+    }
+
+    $resultado = multiplica(3, 2);
+    echo $resultado;
+
+?>
