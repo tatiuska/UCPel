ade978e377d2d62074be7a12df70709e87fc17d7
tatiuska <tfweska@gmail.com>
2024-08-16 20:16:44 -0300

    função anônima - UA 05

---
+++ PBE/2024-08-16_05.php
@@ -0,0 +1,9 @@
+<?php 
+    // função anônima - UA 05
+    $mensagem = function ($nome) {
+        return "Seja bem-vindo, $nome!";
+    };
+
+    echo $mensagem("Cebolinha") . "</br>";
+    echo $mensagem("Mônica") . "</br>";
+?>
