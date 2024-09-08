ced5f8e4ecd375e5b7f0fad784db32081ddb94ee
tatiuska <tfweska@gmail.com>
2024-09-08 13:43:46 -0300

    separação dos dados de conexão para uso tanto de mysqli.php como de pdo.php

---
+++ PBE/UA-08/dados_conexao.php
@@ -0,0 +1,8 @@
+<?php 
+    // criando constantes para os dados de acesso ao banco de dados
+    define("DSN", "mysql");
+    define("SERVIDOR", "localhost");
+    define("USUARIO", "root");
+    define("SENHA", null);
+    define("BANCODEDADOS", "filmerama");
+?>
