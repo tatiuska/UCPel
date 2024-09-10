e8597df150c2a61cc2cecef5743af8fa122355cc
tatiuska <tfweska@gmail.com>
2024-09-10 16:18:40 -0300

    remoção de arquivos pós-transferência.

--- PBE/CRUD/dados_conexao.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    // dados da conexão armazenados em constantes
-    define("DB_HOST", "localhost");
-    define("DB_NAME", "ua9");
-    define("DB_USER", "root");
-    define("DB_PASS", null);
-?>
--- PBE/CRUD/erros.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    // chamando o arquivo de conexao
-    require_once "funcoes.php";
-
-    // tratamento de erros 
-    ini_set('display_errors', true);
-    error_reporting(E_ALL);
-?>
